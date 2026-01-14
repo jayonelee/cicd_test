1e51b84e5eb495e43ee3c145eb1e82f5af43b528
jayone.lee <jayone.lee@camter.co.kr>
2026-01-14 14:54:24 +0900

    cicd 테스트1

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div>테스트 한번 더 </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
